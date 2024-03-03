20cb3da28caae01a157e87b26c85c8802650ddb5
littlemoose93 <littlemoose93@gmail.com>
2024-03-03 15:31:24 -0500

    Added pkcs#7

--- src/set_2/challenge_10.rs
+++ src/set_2/challenge_10.rs
@@ -1,12 +1,16 @@
-use crate::set_1::{aes_128_ecb_decryt, aes_128_ecb_encrypt, xor_exact};
+use crate::{
+    set_1::{aes_128_ecb_decryt, aes_128_ecb_encrypt, xor_exact},
+    set_2::pkcs_7,
+};
 
 pub fn aes_128_cbc_encrypt(iv: &[u8; 16], plain_text: &[u8], key: [u8; 16]) -> Vec<u8> {
     assert!(iv.len() == 16);
-    assert!(plain_text.len() % 16 == 0);
+
+    let padded_plain_text = pkcs_7(plain_text, 16);
     let mut output = Vec::new();
 
     let mut cipher_text = iv.to_vec();
-    for (block_number, chunk) in plain_text.chunks(16).enumerate() {
+    for (block_number, chunk) in padded_plain_text.chunks(16).enumerate() {
         // println!("({block_number}) key: {key:x?}");
         // println!("({block_number}) previous cipher_text: {cipher_text:x?}");
         // println!("({block_number}) chunk: {chunk:x?}");
@@ -34,6 +38,12 @@ pub fn aes_128_cbc_decrypt(iv: &[u8; 16], ciphertext: &[u8], key: [u8; 16]) -> V
         let plain_text = xor_exact(&almost_pt, &previous_cipher_text).unwrap();
         output.extend_from_slice(&plain_text);
         previous_cipher_text = block.to_vec()
+    }
+
+    // remove pkcs#7 padding
+    let bytes_to_remove = *output.last().unwrap();
+    for _ in 0..bytes_to_remove {
+        let _ = output.pop();
     }
 
     output
@@ -64,6 +74,7 @@ mod tests {
 
         let plaintext = aes_128_cbc_decrypt(&iv, &cipher_text, key);
         println!("{}", String::from_utf8_lossy(&plaintext));
+        println!("{:?}", &plaintext);
         assert!(false);
     }
 
@@ -81,9 +92,8 @@ mod tests {
     }
 
     mod aes_128_cbc_encrypt {
-        use crate::set_1::hex_to_bytes;
-
         use super::*;
+        use crate::set_1::hex_to_bytes;
 
         #[test]
         fn known_vectors_16_bytes() {
@@ -140,6 +150,63 @@ mod tests {
             let cipher_text = aes_128_cbc_encrypt(&iv, &plain_text, key);
 
             assert_eq!(expected_cipher_text, cipher_text);
+        }
+    }
+
+    mod aes_128_cbc_encrypt_and_decrypt {
+        use super::*;
+        use crate::set_1::hex_to_bytes;
+
+        #[test]
+        fn known_vectors_17_bytes() {
+            let key: [u8; 16] = hex_to_bytes("2b7e151628aed2a6abf7158809cf4f3c")
+                .unwrap()
+                .try_into()
+                .unwrap();
+            let iv: [u8; 16] = hex_to_bytes("000102030405060708090a0b0c0d0e0f")
+                .unwrap()
+                .try_into()
+                .unwrap();
+
+            let plain_text = hex_to_bytes("6bc1bee22e409f96e93d7e117393172abb").unwrap();
+            let cipher_text = aes_128_cbc_encrypt(&iv, &plain_text, key.clone());
+            let decrypted_plain_text = aes_128_cbc_decrypt(&iv, &cipher_text, key);
+            assert_eq!(decrypted_plain_text, plain_text);
+        }
+
+        #[test]
+        fn known_vectors_32_bytes() {
+            let key: [u8; 16] = hex_to_bytes("2b7e151628aed2a6abf7158809cf4f3c")
+                .unwrap()
+                .try_into()
+                .unwrap();
+            let iv: [u8; 16] = hex_to_bytes("000102030405060708090a0b0c0d0e0f")
+                .unwrap()
+                .try_into()
+                .unwrap();
+
+            let plain_text =
+                hex_to_bytes("6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51")
+                    .unwrap();
+            let cipher_text = aes_128_cbc_encrypt(&iv, &plain_text, key.clone());
+            let decrypted_plain_text = aes_128_cbc_decrypt(&iv, &cipher_text, key);
+            assert_eq!(decrypted_plain_text, plain_text);
+        }
+        #[test]
+        fn known_vectors_48_bytes() {
+            let key: [u8; 16] = hex_to_bytes("2b7e151628aed2a6abf7158809cf4f3c")
+                .unwrap()
+                .try_into()
+                .unwrap();
+            let iv: [u8; 16] = hex_to_bytes("000102030405060708090a0b0c0d0e0f")
+                .unwrap()
+                .try_into()
+                .unwrap();
+
+            let plain_text = hex_to_bytes("6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52ef").unwrap();
+            let cipher_text = aes_128_cbc_encrypt(&iv, &plain_text, key.clone());
+            let decrypted_plain_text = aes_128_cbc_decrypt(&iv, &cipher_text, key);
+            assert_eq!(decrypted_plain_text, plain_text);
         }
     }
 }
